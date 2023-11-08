cc60fd95a3f046faae9b9b4ea00e7d840818e689
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-11-08 23:48:43 +0630

    model add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ModelViewer from './ModelViewer'
 
 const App = () => {
   return (
     <div>
       <h1>Car Model with three.js.</h1>
+      <ModelViewer />
     </div>
   )
 }
---
+++ src/ModelViewer.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react'
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
+import * as THREE from 'three'
+
+const ModelViewer = () => {
+
+    const mount = useRef(null)
+
+    useEffect(() => {
+        const canvas = mount.current;
+        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const scene = new THREE.Scene();
+        const light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+        const model = new THREE.Object3D();
+        scene.add(light);
+        scene.add(camera);
+        scene.add(model);
+    
+        const loader = new GLTFLoader();
+        loader.load('model/scene.gltf', (gltf) => {
+          scene.add(gltf.scene);
+          gltf.animations; // Array<THREE.AnimationClip>
+          gltf.scene; // THREE.Scene
+          gltf.scenes; // Array<THREE.Scene>
+          gltf.cameras; // Array<THREE.Camera>
+          gltf.asset; // Object
+    
+          const animate = () => {
+            requestAnimationFrame(animate);
+            model.rotation.x += 0.01;
+            model.rotation.y += 0.01;
+            renderer.render(scene, camera);
+          };
+    
+          animate();
+        }, undefined, (error) => {
+          console.error('An error occurred:', error);
+        });
+    
+        return () => {
+          mount.current = null;
+        };
+     }, []);
+
+  return (
+    <canvas ref={mount} />
+  )
+}
+
+export default ModelViewer
